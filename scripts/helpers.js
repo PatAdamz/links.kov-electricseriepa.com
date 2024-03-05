aed4a8457919583c73c5b06ea9db913ba987fd75
Patrick <Patrick.adams@erieinsurance.com>
2024-03-04 21:47:54 -0500

    initial commit

---
+++ scripts/helpers.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    setTimeout(function(){
+        document.body.style.backgroundColor = "black";
+        document.body.style.backgroundImage = "none";
+    }, 1500)
+};
